f898897ec5efd2a2269d948c2da3f94de980c56f
Ryan Viegas <rjkviegas@gmail.com>
2020-08-17 15:28:57 +0100

    testNoteGetTextReturnsTextProperty

--- note-model-tests.js
+++ note-model-tests.js
@@ -5,15 +5,12 @@
 
     assert.isTrue(typeof(note.text) === 'string');
   }
+  function testNoteGetTextReturnsTextProperty() {
+    var note = new Note(3);
+
+    assert.isTrue(note.getText() === '3');
+  }
 
   testNoteTextReturnsAString();
-  // function testNoteTextReturnsString() {
-  //   var note = new Note();
-
-  //   if (typeof(note.text) !== String) {
-  //     throw new Error("Note text is not a String");
-  //   }
-  // }
-
-  // testNoteTextReturnsString();
+  testNoteGetTextReturnsTextProperty();
 })(this);
--- note-model.js
+++ note-model.js
@@ -1,4 +1,7 @@
 var Note = function(text) {
     this.text = String(text)
   }
-  
+
+  Note.prototype.getText = function() {
+    return this.text;
+  }
