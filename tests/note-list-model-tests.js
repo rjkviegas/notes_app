9d6345a4b691c282995a08a3d5a8bb98fc95a625
Ryan Viegas <rjkviegas@gmail.com>
2020-08-17 17:24:25 +0100

    made properties private looking

--- tests/note-list-model-tests.js
+++ tests/note-list-model-tests.js
@@ -1,17 +1,15 @@
 (function(exports) {
 
-  function testNoteListNotesIsAnArray() {
-    var notelist = new NoteList();
-
-    assert.isTrue(Array.isArray(notelist.notes));
-  }
-
   function testStoresCreatedNotes() {
     var notelist = new NoteList(Note);
-    notelist.createNote('argument');
 
-    assert.isTrue(notelist.notes[0] instanceof Note);
-    assert.isTrue(notelist.notes[0].text === 'argument');
+    assert.isTrue(notelist.listNotes().length === 0);
+
+    notelist.createNote('argument');
+    
+    assert.isTrue(notelist.listNotes().length === 1);
+    assert.isTrue(notelist.listNotes()[0] instanceof Note);
+    assert.isTrue(notelist.listNotes()[0].getText() === 'argument');
   }
 
   function testListNotesReturnsNotesArray() {
@@ -21,7 +19,6 @@
     assert.isTrue(Array.isArray(notelist.listNotes()));
   }
 
-  testNoteListNotesIsAnArray();
   testStoresCreatedNotes();
   testListNotesReturnsNotesArray();
 })(this);
--- tests/note-model-tests.js
+++ tests/note-model-tests.js
@@ -1,16 +1,10 @@
 (function(exports) {
   
-  function testNoteTextReturnsAString() {
-    var note = new Note(3);
-
-    assert.isTrue(typeof(note.text) === 'string');
-  }
   function testNoteGetTextReturnsTextProperty() {
     var note = new Note(3);
 
     assert.isTrue(note.getText() === '3');
   }
 
-  testNoteTextReturnsAString();
   testNoteGetTextReturnsTextProperty();
 })(this);
