ad38035743c7071dad40e45dc82589b0af3d92e4
Ryan Viegas <rjkviegas@gmail.com>
2020-08-18 17:50:42 +0100

    testNoteListViewNoteListHTMLReturnsNoteListWith0Notes & testNoteListViewNoteListHTMLReturnsNoteListWith2Notes

--- tests/note-list-view-tests.js
+++ tests/note-list-view-tests.js
@@ -1,5 +1,11 @@
 (function(exports) {
   
+  function testNoteListViewNoteListHTMLReturnsNoteListWith0Notes() {
+    var notelist = new NoteList(Note);
+    var notelistview = new NoteListView(notelist);
+
+    assert.isTrue(notelistview.noteListHTML() === '<ul></ul>');
+  }
   function testNoteListViewNoteListHTMLReturnsNoteListWith1Note() {
     var notelist = new NoteList(Note);
     notelist.createNote('Favourite food: pesto');
@@ -8,5 +14,16 @@
     assert.isTrue(notelistview.noteListHTML() === '<ul><li><div>Favourite food: pesto</div></li></ul>');
   }
   
+  function testNoteListViewNoteListHTMLReturnsNoteListWith2Notes() {
+    var notelist = new NoteList(Note);
+    notelist.createNote('Favourite food: pesto');
+    notelist.createNote('Favourite drink: seltzer');
+    var notelistview = new NoteListView(notelist);
+
+    assert.isTrue(notelistview.noteListHTML() === '<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>');
+  }
+  
+  testNoteListViewNoteListHTMLReturnsNoteListWith0Notes()
   testNoteListViewNoteListHTMLReturnsNoteListWith1Note();
+  testNoteListViewNoteListHTMLReturnsNoteListWith2Notes()
 })(this);
