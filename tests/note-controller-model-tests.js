1a5db055c0e4d7f6ff7436bf3ec98d00613e4575
Ryan Viegas <rjkviegas@gmail.com>
2020-08-19 17:28:35 +0100

    assertInnerHTMLOfAppEqualsNoteText

--- tests/note-controller-model-tests.js
+++ tests/note-controller-model-tests.js
@@ -4,6 +4,27 @@
 
     assert.isTrue(noteController instanceof NoteController);
   }
+
+  function assertInnerHTMLOfAppEqualsNoteText() {
+    var NoteDouble = function(text) {
+      this.text = text;
+    }
+    NoteDouble.prototype.getText = function() {
+      return this.text
+    }
+    var noteList = new NoteList(NoteDouble)
+    noteList.listNotes = function() {
+      return [(new NoteDouble('Favourite food: pesto'))];
+    }
+    noteList.createNote = function(text) {
+      return new NoteDouble(text)
+    }
+    var noteController = new NoteController(noteList);
+    noteController.changeApp();
+    
+    assert.isTrue(document.getElementById("app").innerHTML === '<ul><li><div>Favourite food: pesto</div></li></ul>')
+  }
   
+  assertInnerHTMLOfAppEqualsNoteText();
   assertNoteControllerIsAConstructor();
 })(this);
