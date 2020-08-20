75bf187f82ff6ec7e60f2fb93f7e718f18d7ae7d
Ryan Viegas <rjkviegas@gmail.com>
2020-08-20 16:48:27 +0100

    assertSingleNoteViewIsAConstructor

--- tests/single-note-view-tests.js
+++ tests/single-note-view-tests.js
@@ -1,6 +1,10 @@
 (function(exports) {
   
   function assertSingleNoteViewIsAConstructor() {
-    
+    var singleNoteView = new SingleNoteView();
+
+    assert.isTrue(singleNoteView instanceof SingleNoteView)
   }
+
+  assertSingleNoteViewIsAConstructor();
 })(this);
