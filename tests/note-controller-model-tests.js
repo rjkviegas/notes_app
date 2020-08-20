9213934d8a09699a895bb995c6bfed9a453d5095
Ryan Viegas <rjkviegas@gmail.com>
2020-08-20 13:48:54 +0100

    doubled the NoteList class in NoteController test assertInnerHTMLOfAppEqualsNoteText

--- tests/note-controller-model-tests.js
+++ tests/note-controller-model-tests.js
@@ -16,25 +16,25 @@
     NoteDouble.prototype.getText = function() {
       return this.text
     }
-    var noteList = new NoteList(NoteDouble)
-    noteList.listNotes = function() {
+    var NoteListDouble = function(noteClass) {
+      this.noteClass = noteClass;
+    }
+    var noteListDouble = new NoteListDouble(NoteDouble)
+    noteListDouble.listNotes = function() {
       return [(new NoteDouble('Favourite food: pesto'))];
     }
-    // noteList.createNote = function(text) {
-    //   return new NoteDouble("hello world")
-    // }
     var NoteListViewClass = function(notelist) {
       this.notelist = notelist;
     }
     NoteListViewClass.prototype.noteListHTML = function() {
       return '<ul><li><div>Favourite food: pesto</div></li></ul>';
     }
-    var noteController = new NoteController(noteList, NoteListViewClass);
+    var noteController = new NoteController(noteListDouble, NoteListViewClass);
     noteController.changeApp();
     
     assert.isTrue(document.getElementById("app").innerHTML === '<ul><li><div>Favourite food: pesto</div></li></ul>')
   }
   
-  assertInnerHTMLOfAppEqualsNoteText();
   assertNoteControllerIsAConstructor();
+  assertInnerHTMLOfAppEqualsNoteText();
 })(this);
